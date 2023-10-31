32647bb7a13db9fa4c44e9d9f5cea7118303c1b1
Anderson Ritter de Souza <anderdsouza@gmail.com>
2023-10-31 20:42:08 -0300

    update ngtw button

--- src/app/@ngtw/components/button/button.properties.ts
+++ src/app/@ngtw/components/button/button.properties.ts
@@ -44,7 +44,7 @@ export const NGTW_BUTTON_HOST_SELECTOR_CLASS_MAP: { attribute: string; classes: 
       dark: ['text-gray-800', 'border', 'border-gray-800', 'enabled:hover:text-white', 'enabled:hover:bg-gray-800'],
       failure: ['text-red-700', 'enabled:hover:text-white', 'border', 'border-red-700', 'enabled:hover:bg-red-800'],
       info: ['text-blue-700', 'border', 'border-blue-700', 'enabled:hover:text-white', 'enabled:hover:bg-blue-700'],
-      light: ['text-gray-800', 'bg-transparent', 'border', 'border-gray-800', 'enabled:hover:bg-gray-200', 'dark:border-gray-200', 'dark:text-gray-200', 'dark:enabled:hover:text-gray-200'],
+      light: ['text-gray-800', 'bg-transparent', 'border', 'border-gray-800', 'enabled:hover:bg-gray-100', 'dark:border-gray-100', 'dark:text-gray-100', 'dark:enabled:hover:text-gray-100'],
       success: ['text-green-700', 'enabled:hover:text-white', 'border', 'border-green-700', 'enabled:hover:bg-green-700'],
       warning: ['text-yellow-400', 'enabled:hover:text-white', 'border', 'border-yellow-400', 'enabled:hover:bg-yellow-400'],
     }
